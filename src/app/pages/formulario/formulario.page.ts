fda6001e83533a35205cdaa6b4d650f73d7a5128
nyafiu <mtsgonzalezblanco@gmail.com>
2022-12-15 15:28:02 -0600

    a

--- src/app/pages/formulario/formulario.page.ts
+++ src/app/pages/formulario/formulario.page.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { UserService } from 'src/app/services/user.service';
 
+
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.page.html',
@@ -12,6 +14,7 @@ import { UserService } from 'src/app/services/user.service';
 export class FormularioPage implements OnInit {
 
   formulario: FormGroup;
+  uid: string = null;
 
   constructor(private userServices: UserService,
     private loadingCtrl: LoadingController,
@@ -25,11 +28,13 @@ export class FormularioPage implements OnInit {
       marcaAuto: new FormControl(),
       modeloAuto: new FormControl(),
       patenteAuto: new FormControl(),
-      foto: new FormControl()
+      foto: new FormControl(),
+      uid: new FormControl(console.log())
     });
    }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.getUid();
   }
 
   async onSubmit(){
@@ -38,6 +43,16 @@ export class FormularioPage implements OnInit {
     this.showLoading();
     console.log(response);
     this.formulario.reset();
+  }
+
+  async getUid() {
+    const uid = await this.userServices.getUid();
+    if (uid) {
+      this.uid = uid;
+      console.log('uid = ',this.uid);
+    } else {
+      console.log('No existe uid');
+    }
   }
 
   async showLoading() {
