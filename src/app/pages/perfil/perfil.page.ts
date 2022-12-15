4e2509603dee782dfdde2896b268f9affa59cd92
majobc02 <majobc02@gmail.com>
2022-12-15 17:01:19 -0300

    intento perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+import { UserService } from 'src/app/services/user.service';
+import { addDoc } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+import { getDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +15,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  uid: string;
+
+  constructor(private auth: Auth,
+    private userServices: UserService,
+    private firestore: Firestore) { }
 
   ngOnInit() {
+  }
+
+  async getUid() {
+    const uid = await this.userServices.getUid();
+    if (uid) {
+      this.uid = uid;
+      console.log('uid = ',this.uid);
+    } else {
+      console.log('No existe uid');
+    }
+  }
+
+  async getInfoUser(){
+    const path = 'Places'
+    const uid = this.uid;
+    //const docRef = doc(this.firestore, path, uid);
+    //const docSnap = await getDoc(docRef);
+    this.firestore.getDoc(path, uid).subscribe((res) => {
+      console.log('datos son = ',res);
+    })
   }
 
 }
